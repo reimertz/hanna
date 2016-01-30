117edb04928524eeda13c9b16d1b1842b2358295
Pierre Reimertz <pierre@pierrereimertz.com>
2016-01-30 15:37:19 +0100

    fatha

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,17 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('autoprefixer');
+var postcss = require('gulp-postcss');
 var gls = require('gulp-live-server');
 
 gulp.task('sass', function () {
+    var processors = [
+      autoprefixer({browsers: ['last 10 version']})
+    ];
   return gulp.src('*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(processors))
     .pipe(gulp.dest('./.tmp/'));
 });
 
